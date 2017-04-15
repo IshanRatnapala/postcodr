cbbb417ac1169c1097284d980b67fa38f2da9062
Dragfreeze <Dragfreeze@gmail.com>
2017-04-15 14:36:46 +0530

    db methods

--- db.js
+++ db.js
@@ -1,19 +1,25 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(__dirname + '/db/postcodes.db');
 
-function DB_GET (query) {
-    db.get(query, (err, rows) => {
-        if (err) throw err;
+function DB (action, query) {
+    return new Promise(function (resolve, reject) {
+        db[action](query, (err, rows) => {
+            if (err) reject(err);
 
-        console.log(rows);
+            resolve(rows);
+        })
     })
+
 }
 
 module.exports = {
     findByCity: (countryCode, city) => {
-        DB_GET(`SELECT * FROM ${countryCode} WHERE placename="${city}"`);
+        return DB('get', `SELECT * FROM ${countryCode} WHERE placename="${city}"`);
     },
     findByPostcode: (countryCode, postcode) => {
-        DB_GET(`SELECT * FROM ${countryCode} WHERE postalcode="${postcode}"`);
+        return DB('get', `SELECT * FROM ${countryCode} WHERE postalcode="${postcode}"`);
+    },
+    getCities: (countryCode) => {
+        return DB('all', `SELECT placename, postalcode FROM ${countryCode}`);
     }
 };
