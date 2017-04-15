7508c89f09af1d2c90a78ffb00c18f85c1056c7a
Dragfreeze <Dragfreeze@gmail.com>
2017-04-15 18:51:17 +0530

    UI improvements nd cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 $(function () {
     function setPostcode (pickedCity) {
+        $('#error-container').hide();
         $('#city-name').text(pickedCity.placename);
         $('#postcode').text(pickedCity.postalcode);
         $('#postcode-display').show();
         history.pushState(null, null, '/postcode/' + countryCode.toLowerCase() + '/' + pickedCity.placename.toLowerCase());
     }
+
     function clearPostcode () {
         $('#city-name').text('');
         $('#postcode').text('');
@@ -12,48 +14,47 @@ $(function () {
         history.pushState(null, null, '/postcode/' + countryCode.toLowerCase());
     }
 
-
-    //setup dropdowns
-    $('#city-search').on('selected.autocompletr', function (event, data) {
-        var pickedCity = _.filter(cities, function (obj) {
-            return obj.placename === data;
-        });
-        if (pickedCity.length) {
-            setPostcode(pickedCity[0]);
-        } else {
-            clearPostcode();
-        }
-    });
-    $('#city-search').on('blur.autocompletr', function (event, data) {
-        if (!$(this).val().trim().length) {
-            clearPostcode();
-        }
-    });
-    $('#city-search').on('init.autocompletr', function (event) {
-        var inputValue =  _.startCase($(this).val().trim());
-        if (inputValue.length) {
-            $(this).val(inputValue);
+    $('#city-search')
+        .on('selected.autocompletr', function (event, data) {
             var pickedCity = _.filter(cities, function (obj) {
-                return obj.placename === inputValue ;
+                return obj.placename === data;
             });
-            if (pickedCity[0]) {
+            if (pickedCity.length) {
                 setPostcode(pickedCity[0]);
             } else {
-                $('#error-message').text("Couldn't find city. Check spelling.");
+                clearPostcode();
             }
-        } else {
-            $(this).focus();
-        }
-    });
-
-    $('#city-search').autocompletr({
-        closeOnBlur: true,
-        filterFromStart: true,
-        autoComplete: true,
-        dataName: 'placename',
-        minChars: 3,
-        maxHeight: '205px',
-    });
+        })
+        .on('blur.autocompletr', function (event, data) {
+            if (!$(this).val().trim().length) {
+                clearPostcode();
+            }
+        })
+        .on('init.autocompletr', function (event) {
+            var inputValue = _.startCase($(this).val().trim());
+            if (inputValue.length) {
+                $(this).val(inputValue);
+                var pickedCity = _.filter(cities, function (obj) {
+                    return obj.placename === inputValue;
+                });
+                if (pickedCity[0]) {
+                    setPostcode(pickedCity[0]);
+                } else {
+                    $('#error-message').text("Couldn't find city named \"" + inputValue + "\". Check spelling and try again.");
+                    $('#error-container').show();
+                }
+            } else {
+                $(this).focus();
+            }
+        })
+        .autocompletr({
+            closeOnBlur: true,
+            filterFromStart: true,
+            autoComplete: true,
+            dataName: 'placename',
+            minChars: 3,
+            maxHeight: '205px',
+        });
 
     //get user location
 
