75748d9334a090e447650989374709b62afffa33
Dragfreeze <Dragfreeze@gmail.com>
2017-04-15 14:38:17 +0530

    couple more routes

--- server.js
+++ server.js
@@ -6,15 +6,41 @@ const app = express();
 const DB_ACTIONS = require('./db');
 
 app.set('view-engine', 'ejs');
+app.use(express.static(__dirname + '/public', { maxAge: '5d' }));
 
 app.get('/', (req, res) => {
-    res.send('Hello there - This is the home page. You can search for/filter postcodes here.');
+    let countryCode = 'LK';
+    //send the cities for the countries
+
+    DB_ACTIONS.getCities(countryCode)
+        .then((data) => {
+            res.render('pages/index.ejs', {
+                cities: data
+            });
+        })
+        .catch((err) => {
+            throw err;
+        });
 });
 
-app.get('/:countryCode/:city', (req, res) => {
-    res.send('Postcode information for ' + req.params.city);
+app.get('/postcode/:countryCode/:city', (req, res) => {
+    // res.send('Postcode information for ' + req.params.city);
     DB_ACTIONS.findByCity(req.params.countryCode.toUpperCase(), _.startCase(req.params.city));
     DB_ACTIONS.findByPostcode(req.params.countryCode.toUpperCase(), '10290');
+
+    res.render('pages/detail.ejs');
+});
+
+app.get('/ajax/:countryCode', (req, res) => {
+    DB_ACTIONS.getCities(req.params.countryCode)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            throw err;
+        });
+
+
 });
 
 app.listen(PORT, () => {
