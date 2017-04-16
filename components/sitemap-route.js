d7ba3cce3404d70546d25f23a57dac55fee4f308
Dragfreeze <Dragfreeze@gmail.com>
2017-04-16 17:58:20 +0530

    sitemap and sitemap route for dev

---
+++ components/sitemap-route.js
@@ -0,0 +1,34 @@
+const sm = require('sitemap');
+
+module.exports = function (app) {
+    app.get('/sitemap.xml', function (req, res) {
+        let sitemap = sm.createSitemap({
+            hostname: 'https://postcode.world',
+            urls: [{
+                url: '/',
+                changefreq: 'yearly',
+                priority: 0.8
+            }]
+        });
+
+        sitemap.add({
+            url: `https://postcode.world/postcode/lk`,
+            changefreq: 'yearly',
+            priority: 0.9
+        });
+
+        DB_ACTIONS.getCities('LK')
+            .then((data) => {
+                for (var i = 0; i < data.length; i++) {
+                    var placename = encodeURIComponent(data[i].placename);
+                    sitemap.add({
+                        url: `https://postcode.world/postcode/lk/${placename}`,
+                        changefreq: 'yearly',
+                        priority: 1
+                    });
+                }
+                res.header('Content-Type', 'application/xml');
+                res.send(sitemap.toString());
+            });
+    });
+};
